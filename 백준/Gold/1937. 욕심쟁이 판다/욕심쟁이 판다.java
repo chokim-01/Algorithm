a0c958a6135920d0b9ea066a27edb2a668a4aa71
chokim-01 <37997972+chokim-01@users.noreply.github.com>
2022-03-18 05:52:25 +0900

    [Gold III] Title: 욕심쟁이 판다, Time: 1004 ms, Memory: 199932 KB -BaekjoonHub

--- 백준/Gold/1937. 욕심쟁이 판다/욕심쟁이 판다.java
+++ 백준/Gold/1937. 욕심쟁이 판다/욕심쟁이 판다.java
@@ -1,65 +1,53 @@
-import java.util.Arrays;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Main {
-
 	static int N;
-	static int res;
+	static int[][] map;
+	static int[][] memo;
+	static int[][] dxy = { { -1, 0 }, { 1, 0 }, { 0, -1 }, { 0, 1 } };
 
-	static int dxy[][] = { { -1, 0 }, { 0, 1 }, { 1, 0 }, { 0, -1 } };
-
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		Scanner sc = new Scanner(System.in);
-
 		N = sc.nextInt();
-		res = 0;
-		int[][] map = new int[N][N];
-		int[][] mapVisit = new int[N][N];
+		map = new int[N][N];
+		memo = new int[N][N];
+
 		for (int i = 0; i < N; i++)
-			for (int j = 0; j < N; j++) {
+			for (int j = 0; j < N; j++)
 				map[i][j] = sc.nextInt();
-				mapVisit[i][j] = -1;
-			}
 
-		for (int i = 0; i < N; i++)
+		for (int i = 0; i < N; i++) {
 			for (int j = 0; j < N; j++) {
-				int cnt = find(map, i, j, map[i][j], mapVisit);
-				res = res < cnt  ? cnt  : res;
-				
+				dfs(i, j);
 			}
-
-		System.out.println(res);
-
-	}
-
-	static int find(int[][] map, int x, int y, int bamboo, int[][] mapVisit) {
-		if (mapVisit[x][y] != -1 && mapVisit[x][y] != 0)
-			return mapVisit[x][y];
-
-		mapVisit[x][y] = 1;
-		boolean flag = true;
-		int cnt[] = new int[4];
-		for (int i = 0; i < 4; i++) {
-			int nx = x + dxy[i][0];
-			int ny = y + dxy[i][1];
-			if (nx < 0 || ny < 0 || nx >= N || ny >= N)
-				continue;
-			if (bamboo >= map[nx][ny])
-				continue;
-			flag = false;
-			cnt[i] += find(map, nx, ny, map[nx][ny], mapVisit);
-			
-		
-		}
-		Arrays.sort(cnt);
-		mapVisit[x][y] += cnt[3];
-		
-		if (flag) {
-			mapVisit[x][y] = 1;
-			return 1;
 		}
 
-		return mapVisit[x][y];
+		int ans = 0;
+		for (int i = 0; i < N; i++)
+			for (int j = 0; j < N; j++)
+				ans = ans > memo[i][j] ? ans : memo[i][j];
+		System.out.println(ans);
 	}
 
+	static int dfs(int x, int y) {
+		if(memo[x][y] != 0)
+			return memo[x][y];
+		memo[x][y] = 1;
+		
+		for (int d = 0; d < 4; d++) {
+			int nx = x + dxy[d][0];
+			int ny = y + dxy[d][1];
+			if (!mapChk(nx, ny) || map[x][y] >= map[nx][ny])
+				continue;
+			memo[x][y] = Math.max(memo[x][y], dfs(nx, ny)+1);
+		}
+		return memo[x][y];
+	}
+
+	static boolean mapChk(int x, int y) {
+		if (x < 0 || y < 0 || x >= N || y >= N)
+			return false;
+		return true;
+	}
 }
