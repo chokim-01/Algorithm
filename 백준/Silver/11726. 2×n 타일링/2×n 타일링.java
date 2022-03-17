37d27f943b2e719d1be32f82d34a6213c23c016c
chokim-01 <37997972+chokim-01@users.noreply.github.com>
2022-03-18 02:54:15 +0900

    [Silver III] Title: 2×n 타일링, Time: 112 ms, Memory: 12872 KB -BaekjoonHub

--- 백준/Silver/11726. 2×n 타일링/2×n 타일링.java
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.java
@@ -1,4 +1,5 @@
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
@@ -7,25 +8,19 @@ public class Main {
 	public static void main(String[] args) throws IOException {
 		Scanner sc = new Scanner(System.in);
 
-		// 2* N 크기의 직사각형을 1*2, 2*1 의 타일로 채우는 방법의 수
-
 		int N = sc.nextInt();
 
 		dp = new int[N + 1];
-        dp[1]=1;
+		dp[1] = 1;
 		if (N >= 2)
 			dp[2] = 2;
-		
-		for (int i = 3; i <= N; i++)
+		if (N >= 3)
+			dp[3] = 3;
+		for (int i = 4; i <= N; i++)
 			dp[i] = (dp[i - 1] + dp[i - 2])%10007;
+		
+
 		System.out.println(dp[N]);
 
-		// 2 * 2 2
-		// 2 * 3 3
-		// 2 * 4 5
-		// 5 : 8
-		// 6 : 13
-		// 7 : 21
 	}
-
 }
