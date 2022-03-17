5e8e0f916f48343ebed7a1088b56a0746c920272
chokim-01 <37997972+chokim-01@users.noreply.github.com>
2022-03-18 02:36:09 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 116 ms, Memory: 12848 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
@@ -1,47 +1,28 @@
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
 	static int[] dp;
-	static int ans;
-	static int N;
 
 	public static void main(String[] args) throws IOException {
 		Scanner sc = new Scanner(System.in);
 
 		int T = sc.nextInt();
-		// 양수 : 11 이하
-		dp = new int[11];
-		// 1, 2, 3의 합으로 나타내는 방법
-		dp[1] = 1;
-		dp[2] = 2;
-		dp[3] = 4;
+		for (int tc = 1; tc <= T; tc++) {
+			int N = sc.nextInt();
 
-		while (T-- > 0) {
-			N = sc.nextInt();
-			ans = 0;
-			
-			getAns(0);
-			System.out.println(ans);
+			dp = new int[N + 1];
+			dp[1] = 1;
+			if (N >= 2)
+				dp[2] = 2;
+			if (N >= 3)
+				dp[3] = 4;
+			for (int i = 4; i <= N; i++)
+				dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3];
+
+			System.out.println(dp[N]);
 		}
-		
 
 	}
-	static void getAns(int x) {
-		if(x == N) {
-			ans +=1;
-			return;
-		} if(x > N)
-			return;
-		for(int i = 1;i<=3;i++)
-			getAns(x+i);
-			
-	}
-	// true true 02 03 04
-	// 10 true 12 13 14
-	// 20 21 true 23 24
-	// 30 31 32 true 34
-	// 40 41 42 43 true
-	// x 부터 y까지
-
 }
