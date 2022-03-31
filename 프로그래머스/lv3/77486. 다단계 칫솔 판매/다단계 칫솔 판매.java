9b66f124273b5414aafa5618c797fb3eeaf27818
chokim-01 <37997972+chokim-01@users.noreply.github.com>
2022-04-01 05:07:54 +0900

    [level 3] Title: 다단계 칫솔 판매, Time: 12.19 ms, Memory: 133 MB -BaekjoonHub

--- 프로그래머스/lv3/77486. 다단계 칫솔 판매/다단계 칫솔 판매.java
+++ 프로그래머스/lv3/77486. 다단계 칫솔 판매/다단계 칫솔 판매.java
@@ -8,18 +8,19 @@ class Solution {
     public int[] solution(String[] enroll, String[] referral, String[] seller, int[] amount) {
         Map<String, Member> members = new HashMap<>();
 
-        for (String name : enroll
-        ) {
+        for (String name : enroll)
             members.put(name, new Member(name));
-        }
+        
         for (int i=0;  i< members.size(); i++) {
             Member member = members.get(enroll[i]);
-            if (!referral[i].equals("-")){
-            member.setCcin(members.get(referral[i]));}
-            else{
-                member.setCcin(new Member("Minho"));
-            }
+            
+            if (!referral[i].equals("-"))
+                member.ccin = members.get(referral[i]);
+            else
+                member.ccin = new Member("Minho");
+            
         }
+        
         for (int j=0; j<amount.length; j++) {
             Member m = members.get(seller[j]);
             m.plusMoney(amount[j] * 100);
@@ -27,46 +28,32 @@ class Solution {
         int[] answer = {};
 
         answer = new int[enroll.length];
-        for (int k =0; k<enroll.length; k++){
-            answer[k] = members.get(enroll[k]).getMoney();
-        }
+        for (int k =0; k<enroll.length; k++)
+            answer[k] = members.get(enroll[k]).money;
+        
         return answer;
 
     }
 
 
     static class Member {
-        private Member ccin;
-        private String name;
+        Member ccin;
+        String name;
         int money;
+        
+        Member(String name) {
+            this.name = name;
+        };
 
         public void plusMoney(int money) {
             if (ccin != null) {
                 int boonbae = money / 10;
-                this.setMoney(this.getMoney() + money - boonbae);
-                ccin.plusMoney(boonbae);
+                this.money = this.money + money - boonbae;
+                if(boonbae != 0)
+                    ccin.plusMoney(boonbae);
             } else {
-                this.setMoney(this.getMoney() + money);
+                this.money = this.money + money;
             }
-        }
-        public Member(String name) {
-            this.name = name;
-        }
-
-        public Member getCcin() {
-            return ccin;
-        }
-
-        public void setCcin(Member ccin) {
-            this.ccin = ccin;
-        }
-
-        public int getMoney() {
-            return money;
-        }
-
-        public void setMoney(int money) {
-            this.money = money;
         }
     }
 }
