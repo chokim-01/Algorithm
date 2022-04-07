4ce5e6fd32a764f5724033fdf160d4c231a8a798
chokim-01 <37997972+chokim-01@users.noreply.github.com>
2022-04-08 00:45:49 +0900

    [Gold V] Title: 치킨 배달, Time: 384 ms, Memory: 19752 KB -BaekjoonHub

--- 백준/Gold/15686. 치킨 배달/치킨 배달.java
+++ 백준/Gold/15686. 치킨 배달/치킨 배달.java
@@ -1,101 +1,83 @@
-import java.io.IOException;
 import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.Queue;
 import java.util.Scanner;
 
-class Main {
+public class Main {
+	static LinkedList<Node> shops;
+	static LinkedList<Node> house;
+	static int N, M;
+	static int ans;
+
 	static class Node {
-		int x, y, cnt;
+		int x, y;
 
 		public Node(int x, int y) {
 			// TODO Auto-generated constructor stub
+
 			this.x = x;
 			this.y = y;
-			// x, y : 치킨집
+
 		}
 	}
 
-	static List<Node> list;
-	static Queue<Node> q;
-	static int M;
-	static int[][] map;
-	static int res = Integer.MAX_VALUE;
-
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
+		N = sc.nextInt();
+		M = sc.nextInt();
+		ans = Integer.MAX_VALUE;
 
-		int N = sc.nextInt(); // 맵
-		M = sc.nextInt(); // 치킨집 개수 M개를 고름
+		house = new LinkedList<>();
+		shops = new LinkedList<>();
 
-		map = new int[N][N];
-
-		q = new LinkedList<>();
-		list = new LinkedList<>();
-
-		for (int i = 0; i < N; i++)
+		for (int i = 0, tmp; i < N; i++) {
 			for (int j = 0; j < N; j++) {
-				map[i][j] = sc.nextInt();
-				if (map[i][j] == 1)
-					q.add(new Node(i, j));
-				if (map[i][j] == 2) {
-					list.add(new Node(i, j));
-					map[i][j] = 0;
-				}
+				tmp = sc.nextInt();
+				if (tmp == 1)
+					house.add(new Node(i, j));
+				else if (tmp == 2)
+					shops.add(new Node(i, j));
 			}
-		// 집을 큐에 넣음
-		// 치킨집을 리스트에 넣음
-
-		// 치킨 집을 고름 ( 조합 )
-		rec(new LinkedList<>(), 0, 0);
-		System.out.println(res);
-
-	}
-
-	static int find_chicken(List<Node> lst) {
-		Queue<Node> que = new LinkedList<>();
-		for (Node n : q)
-			que.add(n);
-		int cnt = 0;
-		/*
-		 * for (int i = 0; i < map.length; i++)
-		 * System.out.println(Arrays.toString(map[i]));
-		 * 
-		 * System.out.println();
-		 */
-		while (!que.isEmpty()) {
-			int tmp = Integer.MAX_VALUE;
-			Node nq = que.poll();
-			for(Node n :lst) {
-				int val = Math.abs(nq.x-n.x)+Math.abs(nq.y-n.y);
-				if(tmp > val)
-					tmp = val;
-			}
-			cnt += tmp;
-			
 		}
-		return cnt;
+		boolean[] choice = new boolean[shops.size()];
+		comb(0, 0, choice);
+		System.out.println(ans);
 	}
 
-	static void rec(List<Node> tmp_list, int idx, int cnt) {
-		if (cnt == M) {
-			// 조합 돌린거에서 돌림
-			
-			int count = find_chicken(tmp_list);
-			if (res > count)
-				res = count;
+	static int calcChickenD(boolean[] shop) {
+		int allCnt = 0;
+		for (Node h : house) {
+			int cnt = Integer.MAX_VALUE;
+			for (int i = 0; i < shop.length; i++) {
+				if (!shop[i])
+					continue;
+				Node s = shops.get(i);
+				cnt = Math.min(cnt, Math.abs(s.x - h.x) + Math.abs(s.y - h.y));
+				
+			}
+			allCnt += cnt;
+			if(allCnt >= ans)
+				return Integer.MAX_VALUE;
+		}
 
+		return allCnt;
+	}
+
+	static void comb(int index, int choosen, boolean[] choice) {
+
+		if (index == choice.length) {
+			if (choosen == 0)
+				return;
+			int tmp = calcChickenD(choice);
+			ans = ans < tmp ? ans : tmp;
 			return;
 		}
-		if (idx == list.size())
-			return;
-
-		tmp_list.add(new Node(list.get(idx).x, list.get(idx).y));
-		rec(tmp_list, idx + 1, cnt + 1);
-		tmp_list.remove(tmp_list.size() - 1);
-		rec(tmp_list, idx + 1, cnt);
+		if (M - (choosen+1) >= 0) {
+			choice[index] = true;
+			comb(index + 1, choosen + 1, choice);// chose
+			choice[index] = false;
+		}
+		comb(index + 1, choosen, choice); // not chosen
 
 	}
-
 }
