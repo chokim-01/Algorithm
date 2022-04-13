dcaf412d9ecc0aa289308c7a4b4445630018ce7c
chokim-01 <37997972+chokim-01@users.noreply.github.com>
2022-04-13 09:00:54 +0900

    [Gold III] Title: 벽 부수고 이동하기 2, Time: 1260 ms, Memory: 306160 KB -BaekjoonHub

---
+++ 백준/Gold/14442. 벽 부수고 이동하기 2/벽 부수고 이동하기 2.java
@@ -0,0 +1,100 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+	static int N, M, K;
+	static int[][] dxy = { { -1, 0 }, { 1, 0 }, { 0, -1 }, { 0, 1 } };
+
+	public static class Node {
+		int x;
+		int y;
+		int k;
+
+		public Node(int x, int y) {
+			this.x = x;
+			this.y = y;
+			this.k = 0;
+			// TODO Auto-generated constructor stub
+		}
+
+		public Node(int x, int y, int k) {
+			this.x = x;
+			this.y = y;
+			this.k = k;
+			// TODO Auto-generated constructor stub
+		}
+	}
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		K = Integer.parseInt(st.nextToken());
+
+		boolean[][] map = new boolean[N][M];
+		int ans = Integer.MAX_VALUE;
+
+		for (int i = 0; i < N; i++) {
+			String s = br.readLine();
+			for (int j = 0; j < M; j++)
+				map[i][j] = s.charAt(j) == '1' ? false : true;
+		}
+
+		Queue<Node> q = new LinkedList<>();
+
+		int[][] visit = new int[N][M];
+		for (int i = 0; i < N; i++)
+			Arrays.fill(visit[i], 100);
+		q.add(new Node(0, 0));
+		int time = 1;
+
+		outer: while (!q.isEmpty()) {
+			int qsize = q.size();
+
+			for (int i = 0; i < qsize; i++) {
+				Node n = q.poll();
+				if (n.x == N - 1 && n.y == M - 1) {
+					ans = time;
+					break outer;
+				}
+				for (int d = 0; d < 4; d++) {
+					int nx = n.x + dxy[d][0];
+					int ny = n.y + dxy[d][1];
+
+					// 방문 또는 맵 넘어갔을 때
+					if (!mapChk(nx, ny) || n.k >= visit[nx][ny])
+						continue;
+
+					// 벽
+					if (!map[nx][ny] && visit[nx][ny] > n.k + 1) {
+						if (n.k + 1 <= K) {
+							q.add(new Node(nx, ny, n.k + 1));
+							visit[nx][ny] = n.k + 1;
+						}
+
+					}
+					// 벽 아님
+					if (map[nx][ny] && visit[nx][ny] > n.k) {
+						q.add(new Node(nx, ny, n.k));
+						visit[nx][ny] = n.k;
+					}
+				}
+			}
+			time += 1;
+		}
+		System.out.println(ans == Integer.MAX_VALUE ? -1 : ans);
+	}
+
+	static boolean mapChk(int x, int y) {
+		if (x < 0 || y < 0 || x >= N || y >= M)
+			return false;
+		return true;
+
+	}
+}
