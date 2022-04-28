c47ff7ef1bd5210c357efd26481f1ef3dd14481c
chokim-01 <37997972+chokim-01@users.noreply.github.com>
2022-04-28 12:38:22 +0900

    [Gold III] Title: 정사면체, Time: 196 ms, Memory: 15460 KB -BaekjoonHub

--- 백준/Gold/1930. 정사면체/정사면체.java
+++ 백준/Gold/1930. 정사면체/정사면체.java
@@ -1,9 +1,6 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Deque;
-import java.util.LinkedList;
-import java.util.Scanner;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -21,14 +18,17 @@ public class Main {
 
 		int T = Integer.parseInt(br.readLine());
 
+		int[] dice = new int[4];
+		int[] target = new int[4];
+		int[] diceNext = new int[4];
+
 		for (int tc = 1; tc <= T; tc++) {
 			st = new StringTokenizer(br.readLine());
-			int[] dice = new int[4];
-			int[] target = new int[4];
 			for (int i = 0; i < 4; i++)
 				dice[i] = Integer.parseInt(st.nextToken());
 			for (int i = 0; i < 4; i++)
 				target[i] = Integer.parseInt(st.nextToken());
+			// Input
 
 			boolean flag = false;
 
@@ -37,10 +37,11 @@ public class Main {
 					continue;
 				// 0번째 일치
 				// 1,2,3번째를 돌려가며 정답을 찾음.
-				int[] diceNext = new int[4];
-				for(int d = 0;d<4;d++)
+
+				for (int d = 0; d < 4; d++)
 					diceNext[d] = dice[rotate[i][d]];
-				if(equalFind(diceNext,target)) {
+
+				if (equalFind(diceNext, target)) {
 					flag = true;
 					break;
 				}
@@ -48,6 +49,7 @@ public class Main {
 			System.out.println(flag ? 1 : 0);
 		}
 	}
+
 	// 맞는 다이스 찾기
 	static boolean equalFind(int[] dice, int[] target) {
 		// 3번을 미룸
