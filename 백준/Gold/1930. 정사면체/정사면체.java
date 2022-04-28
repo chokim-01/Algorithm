6105b69278fb164dc8b0f92de0cc8b6a96eae635
chokim-01 <37997972+chokim-01@users.noreply.github.com>
2022-04-28 12:50:51 +0900

    [Gold III] Title: 정사면체, Time: 152 ms, Memory: 14928 KB -BaekjoonHub

--- 백준/Gold/1930. 정사면체/정사면체.java
+++ 백준/Gold/1930. 정사면체/정사면체.java
@@ -1,6 +1,8 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -8,12 +10,9 @@ public class Main {
 	static int[][] rotate = { { 0, 1, 2, 3 }, { 1, 0, 3, 2 }, { 2, 1, 3, 0 }, { 3, 1, 0, 2 } };
 	static int[][] change = { { 1, 2, 3 }, { 3, 1, 2 }, { 2, 3, 1 } };
 
-	/*
-	 * static int[] front = { 1, 0, 3, 2 }; static int[] left = { 2, 1, 3, 0 };
-	 * static int[] right = { 3, 1, 0, 2 };
-	 */
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		StringTokenizer st;
 
 		int T = Integer.parseInt(br.readLine());
@@ -46,8 +45,11 @@ public class Main {
 					break;
 				}
 			}
-			System.out.println(flag ? 1 : 0);
+			bw.write(flag?"1":"0");
+			bw.newLine();
 		}
+		bw.flush();
+		bw.close();
 	}
 
 	// 맞는 다이스 찾기
