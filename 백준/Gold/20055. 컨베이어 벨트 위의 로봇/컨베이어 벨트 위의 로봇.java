e91e0b139e056a390870dd815a8fc423f932df07
chokim-01 <37997972+chokim-01@users.noreply.github.com>
2022-06-30 16:04:07 +0900

    [Gold V] Title: 컨베이어 벨트 위의 로봇, Time: 868 ms, Memory: 29448 KB -BaekjoonHub

---
+++ 백준/Gold/20055. 컨베이어 벨트 위의 로봇/컨베이어 벨트 위의 로봇.java
@@ -0,0 +1,91 @@
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+	static int N, K;
+
+	static List<Integer> robot;
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		N = sc.nextInt();
+		K = sc.nextInt(); // K개 이상이면 과정 종료
+		robot = new LinkedList<Integer>();
+		zeroCnt = 0;
+
+		int[] belt = new int[2 * N]; // 내구도
+		boolean[] robotLoc = new boolean[2 * N]; // 로봇 위치
+		for (int i = 0; i < 2 * N; i++)
+			belt[i] = sc.nextInt();
+
+		int ans = 0;
+
+		while (true) {
+			// 1.
+			ans += 1;
+			rotate(belt, robotLoc);
+			move(robotLoc, belt);
+			if (belt[0] != 0) {
+				robot.add(0);
+				belt[0] -= 1;
+				if (belt[0] == 0)
+					zeroCnt += 1;
+				robotLoc[0] = true;
+			}
+
+			if (zeroCnt >= K)
+				break;
+		}
+		System.out.println(ans);
+	}
+
+	static int zeroCnt;
+
+	static void move(boolean[] robotLoc, int[] belt) {
+		for (int i = 0; i < robot.size(); i++) {
+			int now = robot.get(i);
+			int next = (now + 1) % (2 * N);
+			// 다음이 내리는 위치면
+			if (next == N - 1 && belt[next] != 0) {
+				robot.remove(i);
+				robotLoc[now] = false;
+				i--;
+				belt[next] -= 1;
+				if (belt[next] == 0)
+					zeroCnt += 1;
+			}
+			// 옮길 수 있음.
+			else if (belt[next] != 0 && !robotLoc[next]) {
+				belt[next] -= 1;
+				robotLoc[next] = true;
+				robotLoc[now] = false;
+				robot.set(i, next);
+				if (belt[next] == 0)
+					zeroCnt += 1;
+			}
+		}
+
+	}
+
+	static void rotate(int[] belt, boolean[] robotLoc) {
+		for (int i = 0; i < robot.size(); i++) {
+			int now = robot.get(i);
+			int next = (now + 1) % (2 * N);
+			if(next == N-1) {
+				robot.remove(i);
+				robotLoc[now] = false;
+				i--;
+				continue;
+			}
+			robot.set(i, next);
+			robotLoc[now] = false;
+			robotLoc[next] = true;
+		}
+		int tmp = belt[2 * N - 1];
+		for (int i = 2 * N - 1; i > 0; i--)
+			belt[i] = belt[i - 1];
+		belt[0] = tmp;
+	}
+}
