4ee7a03a8c33be429d6d734de46b10923364b246
chokim-01 <37997972+chokim-01@users.noreply.github.com>
2022-11-25 16:50:43 +0900

    [Gold III] Title: 전국시대, Time: 396 ms, Memory: 19036 KB -BaekjoonHub

--- 백준/Gold/15809. 전국시대/전국시대.java
+++ 백준/Gold/15809. 전국시대/전국시대.java
@@ -1,35 +1,27 @@
-import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.DataInputStream;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.PriorityQueue;
-import java.util.StringTokenizer;
 
 public class Main {
     public static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
     public static int N, M;
     public static int[] arr, cost;
-    public static boolean[] visited;
     public static void main(String[] args) throws Exception {
-    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    	StringTokenizer st;
-    	st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
+        FastReader fr = new FastReader();
+        N = fr.nextInt();
+        M = fr.nextInt();
         arr = new int[N + 1];
         cost = new int[N + 1];
         for(int i = 1; i <= N; i++) {
-            cost[i] = Integer.parseInt(br.readLine());
+            cost[i] = fr.nextInt();
             arr[i] = i;
         }
         for(int i = 0; i < M; i++) {
-        	st = new StringTokenizer(br.readLine());
-
-            int command = Integer.parseInt(st.nextToken());
-            int from = Integer.parseInt(st.nextToken());
-            int to = Integer.parseInt(st.nextToken());
+            int command = fr.nextInt();
+            int from = fr.nextInt();
+            int to = fr.nextInt();
             union(from, to, command);
         }
         PriorityQueue<Integer> pq = new PriorityQueue<>();
@@ -56,12 +48,8 @@ public class Main {
                     bw.write(48);
                     size--;
                 }
-                if(div != 0) {
-                    bw.write((current / div) + 48);
-                    current %= div;
-                } else {
-                    bw.write(48);
-                }
+                bw.write((current / div) + 48);
+                current %= div;
                 size--;
             }
             if(s > 1) {
@@ -109,5 +97,104 @@ public class Main {
         }
         return arr[target] = find(arr[target]);
     }
+    public static class FastReader {
+        private final DataInputStream din;
+        private final int BUFFER_SIZE = 1 << 16;
+        private final byte[] buffer;
+        private int bufferPointer, bytesRead;
 
+        public FastReader() {
+            din = new DataInputStream(System.in);
+            buffer = new byte[BUFFER_SIZE];
+            bufferPointer = bytesRead = 0;
+        }
+
+        public String readLine() throws IOException {
+            byte[] buf = new byte[BUFFER_SIZE]; // input line length
+            int cnt = 0, c;
+            while ((c = read()) != -1) {
+                if (c == '\n') {
+                    break;
+                }
+                buf[cnt++] = (byte) c;
+            }
+            return new String(buf, 0, cnt);
+        }
+
+        public int nextInt() throws IOException {
+            int ret = 0;
+            byte c = read();
+            while (c <= ' ') {
+                c = read();
+            }
+            boolean neg = (c == '-');
+            if (neg) {
+                c = read();
+            }
+            do {
+                ret = ret * 10 + c - '0';
+            } while ((c = read()) >= '0' && c <= '9');
+
+            if (neg) {
+                return -ret;
+            }
+            return ret;
+        }
+
+        public long nextLong() throws IOException {
+            long ret = 0;
+            byte c = read();
+            while (c <= ' ')
+                c = read();
+            boolean neg = (c == '-');
+            if (neg)
+                c = read();
+            do {
+                ret = ret * 10 + c - '0';
+            }
+            while ((c = read()) >= '0' && c <= '9');
+            if (neg)
+                return -ret;
+            return ret;
+        }
+
+        public double nextDouble() throws IOException {
+            double ret = 0, div = 1;
+            byte c = read();
+            while (c <= ' ')
+                c = read();
+            boolean neg = (c == '-');
+            if (neg)
+                c = read();
+
+            do {
+                ret = ret * 10 + c - '0';
+            }
+            while ((c = read()) >= '0' && c <= '9');
+
+            if (c == '.') {
+                while ((c = read()) >= '0' && c <= '9') {
+                    ret += (c - '0') / (div *= 10);
+                }
+            }
+
+            if (neg)
+                return -ret;
+            return ret;
+        }
+
+        private void fillBuffer() throws IOException {
+            bytesRead = din.read(buffer, bufferPointer = 0, BUFFER_SIZE);
+            if (bytesRead == -1) {
+                buffer[0] = -1;
+            }
+        }
+
+        private byte read() throws IOException {
+            if (bufferPointer == bytesRead) {
+                fillBuffer();
+            }
+            return buffer[bufferPointer++];
+        }
+    }
 }
