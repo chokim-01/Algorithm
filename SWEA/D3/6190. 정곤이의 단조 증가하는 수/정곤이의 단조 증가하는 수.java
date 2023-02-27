955548672eae0ff6caf82aa40959aafcae646061
chokim-01 <37997972+chokim-01@users.noreply.github.com>
2023-02-27 22:14:52 +0900

    [D3] Title: 정곤이의 단조 증가하는 수, Time: 569 ms, Memory: 35,828 KB -BaekjoonHub

--- SWEA/D3/6190. 정곤이의 단조 증가하는 수/정곤이의 단조 증가하는 수.java
+++ SWEA/D3/6190. 정곤이의 단조 증가하는 수/정곤이의 단조 증가하는 수.java
@@ -1,25 +1,22 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.StringTokenizer;
 
 public class Solution {
-	public static void main(String[] args) throws IOException{
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringBuilder sb = new StringBuilder();
 		StringTokenizer st;
-		
 		int T = Integer.parseInt(br.readLine());
 		for (int t = 1; t <= T; t++) {
 			int N = Integer.parseInt(br.readLine());
 			int max = -1;
 			ArrayList<Integer> arr = new ArrayList<>();
 			st = new StringTokenizer(br.readLine());
-			while(st.hasMoreTokens())
+			while (st.hasMoreTokens())
 				arr.add(Integer.parseInt(st.nextToken()));
 
 			Collections.sort(arr, Collections.reverseOrder());
@@ -35,19 +32,19 @@ public class Solution {
 					}
 				}
 			}
-			bw.write("#"+t+" "+max+"\n");
+			sb.append("#").append(t).append(" ").append(max).append("\n");
 		}
-		bw.flush();
-		bw.close();
+		System.out.println(sb);
 	}
 
 	static boolean Chk(int x) {
-		String s = String.valueOf(x);
-		if (s.charAt(s.length() - 1) == '0')
-			return false;
-		for (int i = 1; i < s.length(); i++)
-			if (s.charAt(i - 1) > s.charAt(i))
+		int before = -1;
+		while(x != 0){
+			before = x % 10;
+			x /= 10;
+			if (x % 10 > before)
 				return false;
+		}
 		return true;
 	}
 }
