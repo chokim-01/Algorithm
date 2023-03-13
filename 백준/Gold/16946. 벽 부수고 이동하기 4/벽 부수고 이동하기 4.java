48a7202f9aff24bf25c8cb10a40064500f36bb29
chokim-01 <37997972+chokim-01@users.noreply.github.com>
2023-03-13 20:02:47 +0900

    [Gold II] Title: 벽 부수고 이동하기 4, Time: 880 ms, Memory: 217492 KB -BaekjoonHub

--- 백준/Gold/16946. 벽 부수고 이동하기 4/벽 부수고 이동하기 4.java
+++ 백준/Gold/16946. 벽 부수고 이동하기 4/벽 부수고 이동하기 4.java
@@ -1,123 +1,101 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.Arrays;
 import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.StringTokenizer;
 
-class Main {
-	static int N, M;
-	static boolean[][] wall;
-	static boolean[][] visit;
-	static int[][] map;
-	static int[][] mapNum;
-	static int num;
-
-	public static void main(String[] args) throws IOException {
+public class Main {
+	static int n, m;
+	static int[][] num, idxs;
+	static boolean[][] visited;
+	static char[][] board;
+	static int[] dx = {1, 0, -1, 0};
+	static int[] dy = {0, 1, 0, -1};
+	public static void main(String[] args) throws NumberFormatException,IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-		String[] st = br.readLine().split(" ");
-		N = Integer.parseInt(st[0]);
-		M = Integer.parseInt(st[1]);
-		wall = new boolean[N][M];
-		visit = new boolean[N][M];
-		map = new int[N][M];
-		mapNum = new int[N][M];
-		num = 1;
-
-		// true : blank | false : wall
-		for (int i = 0; i < N; i++) {
-			String s = br.readLine();
-			for (int j = 0; j < M; j++) {
-				wall[i][j] = (s.charAt(j) - 48 == 0) ? true : false;
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
+		visited = new boolean[n][m];
+		num = new int[n][m];
+		idxs = new int[n][m];
+		board = new char[n][m];
+		
+		for(int i = 0; i < n; i++) {
+			board[i] = br.readLine().toCharArray();
+		}
+		
+		int idx = 1;
+		for(int i = 0; i < n; i++) {
+			for(int j = 0; j < m; j++) {
+				if(board[i][j] == '0' && num[i][j] == 0) {
+					bfs(i, j, idx++);
+				}
 			}
 		}
 		
-		for (int i = 0; i < N; i++) {
-			for (int j = 0; j < M; j++) {
-				if (wall[i][j] && !visit[i][j])
-					bfs(i, j);
+		StringBuilder sb = new StringBuilder();
+		for(int i = 0; i < n; i++) {
+			for(int j = 0; j < m; j++) {
+				if(board[i][j] == '1') {
+					int answer = 1;
+					HashSet<Integer> set = new HashSet<>();
+					for(int k = 0; k < 4; k++) {
+						int nx = i + dx[k];
+						int ny = j + dy[k];
+						
+						if(nx >= 0 && nx < n && ny >= 0 && ny < m && !set.contains(idxs[nx][ny])) {
+							answer += num[nx][ny];
+							set.add(idxs[nx][ny]);
+						}
+					}
+					sb.append(answer % 10);
+				}
+				else {
+					sb.append(0);
+				}
 			}
+			sb.append("\n");
 		}
-		for(int i = 0;i<N;i++) {
-			for(int j = 0;j<M;j++) {
-				if(wall[i][j]) {
-					bw.append("0");
-					continue;
+		System.out.print(sb);
+	}
+	
+	public static void bfs(int x, int y, int idx) {
+		Queue<int[]> queue = new LinkedList<>(), queue2 = new LinkedList<>();
+		
+		visited[x][y] = true;
+		queue.add(new int[] {x, y});
+		
+		int count = 0;
+		while(!queue.isEmpty()) {
+			int[] now = queue.poll();
+			
+			idxs[now[0]][now[1]] = idx;
+			
+			queue2.add(new int[] {now[0], now[1]});
+			
+			count++;
+			
+			for(int i = 0; i < 4; i++) {
+				int nx = now[0] + dx[i];
+				int ny = now[1] + dy[i];
+				
+				if(nx >= 0 && nx < n && ny >= 0 && ny < m) {
+					if(!visited[nx][ny] && board[nx][ny] == '0') {
+						visited[nx][ny] = true;
+						queue.add(new int[] {nx, ny});
+					}
 				}
-				HashSet<Integer> set = new HashSet<Integer>();
-				int cnt = 1;
-				for(int d = 0;d<4;d++) {
-					int nx = i + dxy[d][0];
-					int ny = j + dxy[d][1];
-
-					if(!mapChk(nx, ny))
-						continue;
-					if(set.contains(mapNum[nx][ny]))
-						continue;
-					set.add(mapNum[nx][ny]);
-					cnt += map[nx][ny];
-				}
-				bw.append(String.valueOf(cnt%10));
 			}
-			bw.newLine();
 		}
 		
-		bw.flush();
-		bw.close();
-
-	}
-
-	static int[][] dxy = { { -1, 0 }, { 1, 0 }, { 0, -1 }, { 0, 1 } };
-
-	static class Node {
-		int x, y;
-
-		public Node(int x, int y) {
-			this.x = x;
-			this.y = y;
-			// TODO Auto-generated constructor stub
+		while(!queue2.isEmpty()) {
+			int[] now = queue2.poll();
+			
+			num[now[0]][now[1]] = count;
 		}
-	}
-	static boolean mapChk(int x, int y) {
-		if(x<0 || y<0 || x>=N || y>=M)
-			return false;
-		return true;
-	}
-
-	static void bfs(int x, int y) {
-		// 큐에 추가 하고 끝나면 더.
-		int cnt = 0;
-		Queue<Node> q = new LinkedList<Node>();
-		Queue<Node> qSet = new LinkedList<Node>();
-		q.add(new Node(x, y));
-		qSet.add(new Node(x, y));
-		visit[x][y] = true;
-
-		while (!q.isEmpty()) {
-			Node n = q.poll();
-			cnt +=1 ;
-			for(int d = 0;d<4;d++) {
-				int nx = n.x + dxy[d][0];
-				int ny = n.y + dxy[d][1];
-				if(!mapChk(nx, ny) || visit[nx][ny])
-					continue;
-				if(!wall[nx][ny])
-					continue;
-				visit[nx][ny] = true;
-				q.add(new Node(nx,ny));
-				qSet.add(new Node(nx,ny));
-			}
-		}
-		while(!qSet.isEmpty()) {
-			Node n = qSet.poll();
-			map[n.x][n.y] = cnt;
-			mapNum[n.x][n.y]= num; 
-		}
-		num++;
 	}
 }
