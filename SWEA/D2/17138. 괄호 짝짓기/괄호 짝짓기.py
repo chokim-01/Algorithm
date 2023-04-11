47e64d57346b8315e2e750d873a1f77bf80f1ea5
chokim-01 <37997972+chokim-01@users.noreply.github.com>
2023-04-11 22:49:28 +0900

    [D2] Title: 괄호 짝짓기, Time: 150 ms, Memory: 58,788 KB -BaekjoonHub

--- SWEA/D2/17138. 괄호 짝짓기/괄호 짝짓기.py
+++ SWEA/D2/17138. 괄호 짝짓기/괄호 짝짓기.py
@@ -1,6 +1,20 @@
-if __name__ == "__main__":
-    T = int(input())
-    st = []
-    for t in range(1,T+1):
-        st = input()
-        print(st)
+T = int(input())
+
+for t in range(1, T+1):
+    s = input()
+    stack = []
+    
+    result = "YES"
+    for c in s:
+        if c == '(':
+            stack.append(c)
+        elif c == ')':
+            if not stack:
+                result = "NO"
+                break
+            else:
+                stack.pop()
+    if stack:
+        result = "NO"
+    
+    print(f'#{t} {result}')
