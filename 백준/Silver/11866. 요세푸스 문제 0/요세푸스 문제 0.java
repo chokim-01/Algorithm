6d15485e94d6d081fbbdafc2adce1480dedab178
chokim-01 <37997972+chokim-01@users.noreply.github.com>
2023-04-12 17:36:21 +0900

    [Silver V] Title: 요세푸스 문제 0, Time: 248 ms, Memory: 18708 KB -BaekjoonHub

--- 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.java
+++ 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.java
@@ -1,30 +1,108 @@
-import java.util.Collection;
-import java.util.Iterator;
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
+	static class ArrayQueue {
+		int front;
+		int rear;
+		int size;
+		int[] arrays;
+
+		public ArrayQueue(int size) {
+			// TODO Auto-generated constructor stub
+			this.front = -1;
+			this.rear = -1;
+			this.size = size;
+			this.arrays = new int[size];
+		}
+
+		void enqueue(int number) {
+			if (isFull()) {
+				System.out.println("Full");
+				return;
+			}
+			if (isEmpty()) {
+				front = rear = 0;
+			} else {
+				rear = (rear + 1) % size;
+			}
+			arrays[rear] = number;
+		}
+
+		int dequeue() {
+			int number = 0;
+			if (isEmpty()) {
+				System.out.println("Empty");
+				throw new ArrayIndexOutOfBoundsException();
+			}
+			if (rear == front) {
+				number = arrays[rear];
+				front = rear = -1;
+			} else {
+				number = arrays[front];
+				front = (front + 1) % size;
+			}
+			return number;
+		}
+
+		int sizeOfQueue() {
+			if (isEmpty())
+				return 0;
+			return (rear - front + size) % size + 1;
+		}
+
+		boolean isEmpty() {
+			// true : 비어있다. false : 비어있지않다.
+			if (front == -1 && rear == -1)
+				return true;
+			return false;
+		}
+
+		boolean isFull() {
+			// true : 가득 차있다. false : 가득 차있지 않다.
+			if ((rear + 1) % size == front)
+				return true;
+			return false;
+		}
+
+		void print() {
+			StringBuilder sb = new StringBuilder();
+			for (int i = front; i != rear; i = (i + 1) % size) {
+				sb.append(arrays[i] + " ");
+			}
+			sb.append(arrays[rear]);
+			System.out.println(sb);
+		}
+
+	}
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
+
+		// 값들
 		int N = sc.nextInt();
 		int K = sc.nextInt();
-		Queue<Integer> q = new LinkedList<>();
-		for (int i = 1; i <= N; i++)
-			q.add(i);
+		ArrayQueue q = new ArrayQueue(N);
 
+		for (int i = 1; i <= N; i++) {
+			q.enqueue(i);
+		}
 		StringBuilder sb = new StringBuilder();
 		sb.append("<");
+
+		// 7번 반복
 		while (N-- > 0) {
 			int k = K - 1;
-			while (k-- != 0) {
-				q.add(q.poll());
+			// k-1번만큼 뒤로 보내버려라
+			while (k-- > 0) {
+				q.enqueue(q.dequeue());
 			}
-			if (q.size() == 1) {
-				sb.append(q.poll());
-			} else
-				sb.append(q.poll() + ", ");
+			int number = q.dequeue();
+			if (q.sizeOfQueue() == 0) {
+				sb.append(number);
+			} else {
+				sb.append(number + ", ");
+			}
 		}
 		sb.append(">");
 		System.out.println(sb);
