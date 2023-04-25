f9efb90157ce685695c7307170ac68e16e1e44c7
chokim-01 <37997972+chokim-01@users.noreply.github.com>
2023-04-25 18:20:58 +0900

    [Bronze III] Title: 소금 폭탄, Time: 136 ms, Memory: 14696 KB -BaekjoonHub

---
+++ 백준/Bronze/13223. 소금 폭탄/소금 폭탄.java
@@ -0,0 +1,45 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.PriorityQueue;
+import java.util.stream.Stream;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+
+		int[] s1 = Stream.of(br.readLine().split(":")).mapToInt(Integer::parseInt).toArray();
+		int[] s2 = Stream.of(br.readLine().split(":")).mapToInt(Integer::parseInt).toArray();
+		s2[0] += 24;
+		int[] ans = new int[3];
+		ans[0] = s2[0] - s1[0];
+		ans[1] = s2[1] - s1[1];
+		if (ans[1] < 0) {
+			ans[0] -= 1;
+			ans[1] += 60;
+		}
+		ans[2] = s2[2] - s1[2];
+		if (ans[2] < 0) {
+			ans[1] -= 1;
+			ans[2] += 60;
+		}
+		if (ans[1] < 0) {
+			ans[0] -= 1;
+			ans[1] += 60;
+		}
+		if (ans[0] >= 24)
+			ans[0] -= 24;
+		if (ans[0] == 0 && ans[1] == 0 && ans[2] == 0)
+			ans[0] = 24;
+		int cnt = 0;
+		for (int a : ans) {
+			System.out.printf("%02d", a);
+			if (cnt++ < 2)
+				System.out.print(":");
+		}
+	}
+}
