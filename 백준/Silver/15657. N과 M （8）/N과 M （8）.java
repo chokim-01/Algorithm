fdf99c5dc983b39ded7c296bf7c0c4de6d52412d
chokim-01 <37997972+chokim-01@users.noreply.github.com>
2023-05-11 02:01:53 +0900

    [Silver III] Title: N과 M (8), Time: 228 ms, Memory: 20540 KB -BaekjoonHub

---
+++ 백준/Silver/15657. N과 M （8）/N과 M （8）.java
@@ -0,0 +1,42 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+import java.util.stream.Stream;
+
+public class Main {
+	static int N, M;
+	static int[] numbers;
+	static StringBuilder ans;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		numbers = Stream.of(br.readLine().split(" ")).mapToInt(Integer::parseInt).sorted().toArray();
+		ans = new StringBuilder();
+		dfs(new int[M], 0);
+		System.out.println(ans);
+
+	}
+
+	static void dfs(int[] choice, int cnt) {
+		if (cnt == M) {
+			for (int c : choice)
+				ans.append(c + " ");
+			ans.append("\n");
+			return;
+		}
+		for (int i = 0; i < numbers.length; i++) {
+			if (cnt != 0)
+				if (numbers[i] < choice[cnt - 1])
+					continue;
+			choice[cnt] = numbers[i];
+			dfs(choice, cnt + 1);
+			choice[cnt] = 0;
+		}
+	}
+
+}
