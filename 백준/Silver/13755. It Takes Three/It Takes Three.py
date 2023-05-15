cf4031708eabea9263cf6805f42ca22b5887d6cf
chokim-01 <37997972+chokim-01@users.noreply.github.com>
2023-05-16 08:08:48 +0900

    [Silver III] Title: It Takes Three, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/13755. It Takes Three/It Takes Three.py
+++ 백준/Silver/13755. It Takes Three/It Takes Three.py
@@ -1,20 +1,10 @@
-rectangles=[list(map(int, input().split())) for i in range(3)]
-ans=0
-
-def is_valid():
-    r1,r2,r3 = sorted(rectangles)
-    if r1[0]+r2[0]!=r3[0] or r1[1]!=r2[1] or r1[1]+r3[1]!=r3[0]:
-        return 0
-    return 1
-
-def solve(k):
-    if k==3:
-        return is_valid()
-    if solve(k+1):
-        return 1
-    rectangles[k][0],rectangles[k][1]=rectangles[k][1],rectangles[k][0]
-    if solve(k+1):
-        return 1
-    return 0
-
-print(solve(0))
+r=[list(map(int,input().split()))for i in range(3)];f=0
+def v():
+    a,b,c=sorted(r);return a[0]+b[0]==c[0]and a[1]==b[1]and a[1]+c[1]==c[0]
+def s(k):
+    if k==3:return v()
+    ret=s(k+1)
+    r[k][0],r[k][1]=r[k][1],r[k][0]
+    ret|=s(k+1)
+    return ret
+print(int(s(0)))
