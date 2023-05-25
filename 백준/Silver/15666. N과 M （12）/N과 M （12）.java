e0fb7acdac9044fd40cfd48f9e78ad854a0a6d7d
chokim-01 <37997972+chokim-01@users.noreply.github.com>
2023-05-25 17:08:31 +0900

    [Silver II] Title: N과 M (12), Time: 188 ms, Memory: 17660 KB -BaekjoonHub

---
+++ 백준/Silver/15666. N과 M （12）/N과 M （12）.java
@@ -0,0 +1,51 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+import java.util.stream.Stream;
+
+public class Main {
+	static int N, M;
+	static int[] arr;
+	static StringBuilder sb;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+
+		arr = Stream.of(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+		Arrays.sort(arr);
+		sb = new StringBuilder();
+		dfs(0, new int[M], new boolean[N]);
+
+		System.out.println(sb);
+	}
+
+	static void dfs(int index, int[] choice, boolean[] visit) {
+		if (index == M) {
+			for (int c : choice)
+				sb.append(c + " ");
+			sb.append("\n");
+
+			return;
+		}
+		int before = -1;
+		for (int i = 0; i < N; i++) {
+			if ((index != 0 && choice[index - 1] > arr[i]) || before == arr[i])
+				continue;
+			before = arr[i];
+			choice[index] = arr[i];
+
+			dfs(index + 1, choice, visit);
+		}
+	}
+
+	static void swap(int a, int b) {
+		int tmp = arr[a];
+		arr[a] = arr[b];
+		arr[b] = tmp;
+	}
+}
