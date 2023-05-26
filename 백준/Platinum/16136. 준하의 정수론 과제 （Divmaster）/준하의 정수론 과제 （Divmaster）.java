83de8a901654749305749f8f480720e8cc7f2f37
chokim-01 <37997972+chokim-01@users.noreply.github.com>
2023-05-26 22:13:40 +0900

    [Platinum II] Title: 준하의 정수론 과제 (Divmaster), Time: 860 ms, Memory: 104824 KB -BaekjoonHub

--- 백준/Platinum/16136. 준하의 정수론 과제 （Divmaster）/준하의 정수론 과제 （Divmaster）.java
+++ 백준/Platinum/16136. 준하의 정수론 과제 （Divmaster）/준하의 정수론 과제 （Divmaster）.java
@@ -7,7 +7,8 @@ import java.util.stream.Stream;
 
 public class Main {
 	static int N, M;
-	static int[] ans, tree;
+	static int[] ans;
+	static long[][] tree;
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -19,66 +20,71 @@ public class Main {
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
 		int[] works = Stream.of(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-		tree = new int[works.length << 1];
-		treeInit(works);
+		tree = new long[works.length << 2][2];
 
+		init(1, 1, N, works);
 		while (M-- > 0) {
 			st = new StringTokenizer(br.readLine());
 			int a = Integer.parseInt(st.nextToken());
-			int b = Integer.parseInt(st.nextToken()) - 1;
-			int c = Integer.parseInt(st.nextToken()) - 1;
+			int b = Integer.parseInt(st.nextToken());
+			int c = Integer.parseInt(st.nextToken());
 
 			if (a == 1) { // change
-				for (int i = b; i <= c; i++) {
-					if (tree[i + N] < 3)
-						continue;
-					update(i);
-				}
+				update(1, 1, N, b, c);
 			} else { // query
-				answer.append(query(b, c)).append("\n");
+				answer.append(query(1, 1, N, b, c)).append("\n");
 			}
 		}
+
 		System.out.println(answer);
 	}
 
-	static void update(int v) {
-		v = v + N;
-		int val = tree[v] - ans[tree[v]];
-		do
-			tree[v] -= val;
-		while ((v >>= 1) != 0);
+	static long query(int node, int l, int r, int a, int b) {
+		if (b < l || r < a)
+			return 0;
+		if (a <= l && r <= b) {
+			return tree[node][0];
+		}
+		int mid = (l + r) / 2;
+		return query(node * 2, l, mid, a, b) + query(node * 2 + 1, mid + 1, r, a, b);
+
 	}
 
-	static int query(int l, int r) {
-		int res = 0;
-		l = l + N;
-		r = r + N + 1;
-		for (; l < r; l >>= 1, r >>= 1) {
-			if ((l & 1) == 1) {
-				res += tree[l];
-				l++;
-			}
-			if ((r & 1) == 1) {
-				r--;
-				res += tree[r];
-			}
-		}
-		return res;
+	static long[] update(int node, int l, int r, int a, int b) {
+		if (b < l || r < a)
+			return tree[node];
+
+		if (tree[node][1] < 3)
+			return tree[node];
+
+		if (l == r)
+			return tree[node] = new long[] { ans[(int) tree[node][0]], ans[(int) tree[node][0]] };
+
+		int mid = (l + r) / 2;
+		long[] left = update(node * 2, l, mid, a, b);
+		long[] right = update(node * 2 + 1, mid + 1, r, a, b);
+
+		tree[node][0] = left[0] + right[0];
+		tree[node][1] = Math.max(left[1], right[1]);
+		return tree[node];
 	}
 
-	static void treeInit(int[] works) {
-		for (int i = N; i < tree.length; i++) {
-			int n = i;
-			do
-				tree[n] += works[i - N];
-			while ((n = n >> 1) != 0);
-		}
+	static long[] init(int node, int l, int r, int[] works) {
+		if (l == r)
+			return tree[node] = new long[] { works[l - 1], works[l - 1] };
+
+		int mid = (l + r) / 2;
+		long[] left = init(node * 2, l, mid, works);
+		long[] right = init(node * 2 + 1, mid + 1, r, works);
+		tree[node][0] = left[0] + right[0];
+		tree[node][1] = Math.max(left[1], right[1]);
+		return tree[node];
 	}
 
 	static void makeMeasure() {
 		N = 1000001;
 		ans = new int[N];
-		int[] measure = new int[N]; // 소인수 중 가장 작은
+		int[] measure = new int[N];
 		int[] measureCounts = new int[N];
 
 		// che
