c88c635870960f28ff1e54c85d197319012011de
chokim-01 <37997972+chokim-01@users.noreply.github.com>
2023-06-07 17:34:58 +0900

    [Platinum V] Title: 이항 계수와 쿼리, Time: 560 ms, Memory: 82220 KB -BaekjoonHub

--- 백준/Platinum/13977. 이항 계수와 쿼리/이항 계수와 쿼리.java
+++ 백준/Platinum/13977. 이항 계수와 쿼리/이항 계수와 쿼리.java
@@ -1,10 +1,5 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.LinkedList;
-import java.util.List;
 import java.util.StringTokenizer;
 
 class Main {
