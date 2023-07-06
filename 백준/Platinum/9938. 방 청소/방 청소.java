34df10b968942307a37505e13331d77a4f1a663d
chokim-01 <37997972+chokim-01@users.noreply.github.com>
2023-07-06 11:09:30 +0900

    [Platinum III] Title: 방 청소, Time: 1064 ms, Memory: 106760 KB -BaekjoonHub

--- 백준/Platinum/9938. 방 청소/방 청소.java
+++ 백준/Platinum/9938. 방 청소/방 청소.java
@@ -1,10 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Stack;
 import java.util.StringTokenizer;
 
 public class Main {
