b33a23a82dc964a4ce7b830c26f6aa00b4f55880
chokim-01 <37997972+chokim-01@users.noreply.github.com>
2023-07-28 02:47:18 +0900

    [Gold V] Title: 김밥천국의 계단, Time: 244 ms, Memory: 21504 KB -BaekjoonHub

--- 백준/Gold/28069. 김밥천국의 계단/김밥천국의 계단.java
+++ 백준/Gold/28069. 김밥천국의 계단/김밥천국의 계단.java
@@ -2,22 +2,18 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
-	static int N, M, K;
+	static int N, K;
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		N = sc.nextInt();
 		K = sc.nextInt();
-		M = 1000001;
-		int[] min = new int[M];
-		Arrays.fill(min, M);
-		min[1] = 1;
-		min[2] = 2;
-		min[3] = 3;
-
-		for (int i = 3; i < M; i++) {
+		int[] min = new int[N + 1];
+		Arrays.fill(min, N);
+		min[0] = 0;
+		for (int i = 1; i <= N; i++) {
 			min[i] = Math.min(min[i], min[i - 1] + 1);
-			if (i + i / 2 < M)
+			if (i + i / 2 <= N)
 				min[i + i / 2] = Math.min(min[i] + 1, min[i + i / 2]);
 		}
 		System.out.println(min[N] <= K ? "minigimbob" : "water");
