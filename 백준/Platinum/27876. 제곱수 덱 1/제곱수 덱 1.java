97789c49e99117d98dfd93f11c0068739b0199c6
chokim-01 <37997972+chokim-01@users.noreply.github.com>
2023-08-17 01:20:41 +0900

    [Platinum IV] Title: 제곱수 덱 1, Time: 288 ms, Memory: 17728 KB -BaekjoonHub

--- 백준/Platinum/27876. 제곱수 덱 1/제곱수 덱 1.java
+++ 백준/Platinum/27876. 제곱수 덱 1/제곱수 덱 1.java
@@ -1,43 +1,4 @@
-import java.util.ArrayList;
 import java.util.Scanner;
-
-class UF {
-	int[] parent, rank;
-
-	public UF(int n) {
-		parent = new int[n + 1];
-		rank = new int[n + 1];
-		for (int i = 1; i <= n; i++) {
-			parent[i] = i;
-			rank[i] = 1;
-		}
-	}
-
-	public int find(int x) {
-		if (parent[x] != x) {
-			parent[x] = find(parent[x]);
-		}
-		return parent[x];
-	}
-
-	public void union(int x, int y) {
-		int rootX = find(x);
-		int rootY = find(y);
-
-		if (rootX == rootY) {
-			return;
-		}
-
-		if (rank[rootX] > rank[rootY]) {
-			parent[rootY] = rootX;
-		} else if (rank[rootX] < rank[rootY]) {
-			parent[rootX] = rootY;
-		} else {
-			parent[rootY] = rootX;
-			rank[rootX]++;
-		}
-	}
-}
 
 public class Main {
 	static final int mod = 998244353;
@@ -46,33 +7,18 @@ public class Main {
 		Scanner sc = new Scanner(System.in);
 		int N = sc.nextInt();
 
-		UF uf = new UF(N);
+		long ans = 29030400;
 
-		ArrayList<Integer> nums[] = new ArrayList[2];
-		for (int i = 0; i < 2; i++)
-			nums[i] = new ArrayList<>();
-		long ans = 1, cnt = 0;
-		int nm = 1;
-		int n = 2;
-		while ((nm = n * n++) < 2 * N)
-			nums[nm % 2].add(nm);
-
-		outer: for (int diff = 1, a, b; diff < N; diff++) {
-			for (int i = 0, half; i < nums[diff % 2].size(); i++) {
-				half = nums[diff % 2].get(i) >> 1;
-				a = half - diff / 2;
-				b = half + diff / 2 + (diff % 2 == 1 ? 1 : 0);
-				if (a <= 0 || b > N)
-					continue;
-				if (uf.find(a) != uf.find(b)) {
-					uf.union(a, b);
-					ans = (ans * diff) % mod;
-					cnt++;
-				}
-				if (cnt == N - 1)
-					break outer;
+		if (N == 1)
+			ans = 1;
+		else if (N < 14)
+			ans = -1;
+		else {
+			for (int i = 15; i <= N; i++) {
+				long b = (long) (Math.sqrt(i * 2 - 1));
+				ans = (ans * ((i << 1) - b * b)) % mod;
 			}
 		}
-		System.out.println(cnt == N - 1 ? ans : -1);
+		System.out.println(ans);
 	}
 }
