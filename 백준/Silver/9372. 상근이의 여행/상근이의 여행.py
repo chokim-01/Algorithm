f707922f3e8872554bcd44aaa25271823081d1e6
chokim-01 <37997972+chokim-01@users.noreply.github.com>
2023-12-09 10:59:24 +0900

    [Silver IV] Title: 상근이의 여행, Time: 60 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/9372. 상근이의 여행/상근이의 여행.py
+++ 백준/Silver/9372. 상근이의 여행/상근이의 여행.py
@@ -2,6 +2,5 @@ import sys
 input = sys.stdin.readline
 for _ in range(int(input())):
     n, m = map(int, input().split())
-    for _ in range(m):
-        input()
-    print(n - 1)
+    [input() for _ in range(m)]
+    print(n-1)
