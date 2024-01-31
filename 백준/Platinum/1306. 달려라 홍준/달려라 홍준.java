04bfdf95b10a4e13b8b7d7f882a2e64ea6def626
chokim-01 <37997972+chokim-01@users.noreply.github.com>
2024-01-31 09:33:37 +0900

    [Platinum V] Title: 달려라 홍준, Time: 888 ms, Memory: 151100 KB -BaekjoonHub

--- 백준/Platinum/1306. 달려라 홍준/달려라 홍준.java
+++ 백준/Platinum/1306. 달려라 홍준/달려라 홍준.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 import java.util.stream.Stream;
 
@@ -28,16 +29,45 @@ public class Main {
 
 		int query(int a, int b) {
 			int ret = 0;
-			
+
 			int bef = a;
 			for (int now = bef + (bef & -bef); now <= b; bef = now, now += now & -now)
 				ret = Math.max(ret, tree[bef][1]);
-			
+
 			ret = Math.max(ret, nums[bef]);
-			
+
 			bef = b;
 			for (int now = bef - (bef & -bef); now >= a; bef = now, now -= now & -now)
 				ret = Math.max(ret, tree[bef][0]);
+			return ret;
+		}
+	}
+
+	static class Seg {
+		int[] tree;
+
+		public Seg() {
+			// TODO Auto-generated constructor stub
+			tree = new int[MAX << 1];
+		}
+
+		void update(int x, int v) {
+			x += MAX;
+			tree[x] = v;
+			while ((x >>= 1) != 0)
+				tree[x] = Math.max(tree[x << 1], tree[x << 1 | 1]);
+		}
+
+		int query(int a, int b) {
+			a += MAX;
+			b += MAX;
+			int ret = 0;
+			for (; a < b; a >>= 1, b >>= 1) {
+				if ((a & 1) == 1)
+					ret = Math.max(ret, tree[a++]);
+				if ((b & 1) == 1)
+					ret = Math.max(ret, tree[--b]);
+			}
 			return ret;
 		}
 	}
@@ -51,16 +81,25 @@ public class Main {
 		MAX = N + 1;
 
 		nums = Stream.of((("0 ") + br.readLine()).split(" ")).mapToInt(Integer::parseInt).toArray();
-
-		FWick fwick = new FWick();
-
-		for (int i = 1; i <= N; i++) {
-			fwick.update(i, nums[i]);
-			fwick.update2(i, nums[i]);
-		}
 		StringBuilder sb = new StringBuilder();
+
+//		FWick fwick = new FWick();
+//
+//		for (int i = 1; i <= N; i++) {
+//			fwick.update(i, nums[i]);
+//			fwick.update2(i, nums[i]);
+//		}
+//		for (int i = M; i <= N - M + 1; i++)
+//			sb.append(fwick.query(i - M + 1, i + M - 1)).append(" ");
+
+		Seg seg = new Seg();
+		
+		for (int i = 1; i <= N; i++)
+			seg.update(i, nums[i]);
+
 		for (int i = M; i <= N - M + 1; i++)
-			sb.append(fwick.query(i - M + 1, i + M - 1)).append(" ");
+			sb.append(seg.query(i - M + 1, i + M)).append(" ");
+
 		System.out.println(sb);
 	}
 }
