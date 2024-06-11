156805d814870321c9a4242e07ebe7b4f4964b13
chokim-01 <37997972+chokim-01@users.noreply.github.com>
2024-06-11 20:12:26 +0900

    [Gold III] Title: 소용돌이 예쁘게 출력하기, Time: 192 ms, Memory: 16764 KB -BaekjoonHub

---
+++ 백준/Gold/1022. 소용돌이 예쁘게 출력하기/소용돌이 예쁘게 출력하기.java
@@ -0,0 +1,63 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Queue;
+import java.util.stream.Stream;
+
+public class Main {
+
+    static int[] start_v;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int[] xy = Stream.of(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+
+        Queue<Integer> a_stack = new ArrayDeque<>();
+
+        start_v = new int[5002];
+        start_v[0] = 1;
+        for (int i = 1; i < start_v.length; i++) {
+            int v = (i << 1) - 1;
+            start_v[i] = v * v + 1;
+        }
+
+        int max = 0;
+        for (int i = xy[0]; i <= xy[2]; i++) {
+            for (int j = xy[1]; j <= xy[3]; j++) {
+                int p_max = Math.max(Math.abs(i), Math.abs(j));
+                int c = calc(i, j, p_max);
+                a_stack.add(c);
+                max = max < c ? c : max;
+            }
+        }
+
+        String format = "%" + String.valueOf(max).length() + "d ";
+
+        StringBuilder ans = new StringBuilder();
+        for (int i = xy[0]; i <= xy[2]; i++) {
+            for (int j = xy[1]; j <= xy[3]; j++)
+                ans.append(String.format(format, a_stack.poll()));
+            ans.append("\n");
+        }
+        System.out.println(ans);
+
+    }
+
+    static int calc(int x, int y, int m) {
+        if (m == 0)
+            return 1;
+        if (y == m && x == m)
+            return start_v[m + 1] - 1;
+
+        int ret = start_v[m] - 1;
+        if (y == m)
+            return ret + m - x;
+        if (x == -m)
+            return ret + 2 * m + (m - y);
+        if (y == -m)
+            return ret + 4 * m + (m + x);
+        return ret + 6 * m + (m + y);
+    }
+}
