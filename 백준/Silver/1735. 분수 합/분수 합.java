07e1358fe6e09462f6a39b2fcdb6bb826ad7daa2
chokim-01 <37997972+chokim-01@users.noreply.github.com>
2024-08-01 04:00:29 +0900

    [Silver III] Title: 분수 합, Time: 76 ms, Memory: 11496 KB -BaekjoonHub

--- 백준/Silver/1735. 분수 합/분수 합.java
+++ 백준/Silver/1735. 분수 합/분수 합.java
@@ -2,10 +2,11 @@ import java.io.*;
 import java.util.StringTokenizer;
 
 public class Main {
-    public static void fraction_sum(int[] A, int[] B) {
+    public static void fractionSum(int[] A, int[] B) {
         StringBuilder sb = new StringBuilder();
 
-        // 굳이 0으로 초기화하고 다시 변수에 값을 집어넣을 필요는 없을 것 같습니다.
+        // 굳이 분모가 같은 경우는 제한을 걸 필요 없고, 
+        // 0으로 초기화하고 다시 변수에 값을 집어넣을 필요는 없을 것 같습니다.
         int numerator = (A[0] * B[1]) + (A[1] * B[0]); // 분자
         int denominator = B[0] * B[1]; // 분모
         int gcdValue = findGCD(numerator, denominator); // gcd 값
@@ -16,7 +17,7 @@ public class Main {
     }
 
     // 최대공약수
-    // 반복문으로 작성해도 현재 코드보다 간결하게 작성할 수 있을 것 같네요
+    // 작성하신 것 처럼 반복문으로 작성해도 현재 코드보다 간결하게 작성할 수 있을 것 같네요
     static int findGCD(long A, long B) {
         long max = Math.max(A, B);
         long min = Math.min(A, B);
@@ -33,6 +34,7 @@ public class Main {
     // A,B는 사용되지 않는 변수 같습니다.
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
         int[] numerator = new int[2];
         int[] denominator = new int[2];
 
@@ -42,6 +44,7 @@ public class Main {
             denominator[i] = Integer.parseInt(st.nextToken());
         }
 
-        fraction_sum(numerator, denominator);
+        fractionSum(numerator, denominator);
     }
 }
+// 함수 내에서도 역할마다 개행으로 구분하는게 좋을 것 같네요
