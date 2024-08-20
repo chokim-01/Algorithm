d55d7a7c9f184bc90efed10f0008cf33923504bc
chokim-01 <37997972+chokim-01@users.noreply.github.com>
2024-08-20 15:49:07 +0900

    [Gold I] Title: 다리 만들기 2, Time: 112 ms, Memory: 14640 KB -BaekjoonHub

--- 백준/Gold/17472. 다리 만들기 2/다리 만들기 2.java
+++ 백준/Gold/17472. 다리 만들기 2/다리 만들기 2.java
@@ -1,183 +1,164 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.PriorityQueue;
-import java.util.Queue;
 import java.util.StringTokenizer;
+import java.util.function.BiFunction;
+import java.util.stream.Stream;
 
-class Main {
+public class Main {
+    static int N, M;
+    static int[][] map, link;
+    static int[][] dxy = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+    static boolean[] v;
+    static int ans;
 
-	static int N, M;
-	static int[][] map;
-	static int[][] edges;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        input(br);
 
-	static int[][] dxy = { { -1, 0 }, { 1, 0 }, { 0, -1 }, { 0, 1 } };
+        mapNumbering();
 
-	static class Node implements Comparable<Node> {
-		int x, y, d, cnt;
+        linkIslandsHorizontal();
+        linkIslandsVertical();
 
-		public Node(int x) {
-			// TODO Auto-generated constructor stub
-			this.x = x;
-		}
+        ans = link.length == 2 ? 0 : Integer.MAX_VALUE;
 
-		public Node(int x, int y) {
-			// TODO Auto-generated constructor stub
-			this.x = x;
-			this.y = y;
-		}
+        for (int start = 1; start < link.length; start++) {
+            v = new boolean[link.length];
+            v[start] = true;
+            combination(0, 0, new ArrayList<>(Arrays.asList(start)));
+            v[start] = false;
+        }
 
-		public Node(int x, int y, int d, int cnt) {
-			// TODO Auto-generated constructor stub
-			this.x = x;
-			this.y = y;
-			this.d = d;
-			this.cnt = cnt;
-		}
+        System.out.println(ans == Integer.MAX_VALUE ? -1 : ans);
+    }
 
-		@Override
-		public int compareTo(Node o) {
-			// TODO Auto-generated method stub
-			return this.y - o.y;
-		}
+    static void combination(int depth, int cost, ArrayList<Integer> choice) {
+        if (depth == link.length - 2) {
+            ans = Math.min(ans, cost);
+            return;
+        }
 
-	}
+        for (int now : new ArrayList<>(choice)) {
+            for (int next = 1; next < link.length; next++) {
+                if (link[now][next] == Integer.MAX_VALUE || v[next])
+                    continue;
 
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
+                v[next] = true;
+                choice.add(next);
+                combination(depth + 1, cost + link[now][next], choice);
+                choice.remove(choice.size() - 1);
+                v[next] = false;
+            }
+        }
+    }
 
-		map = new int[N][M];
 
-		for (int i = 0; i < N; i++) {
-			String[] ss = br.readLine().split(" ");
-			for (int j = 0; j < M; j++) {
-				map[i][j] = Integer.parseInt(ss[j]);
-			}
-		}
+    static void linkIslandsHorizontal() {
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                if (map[i][j] == 0)
+                    continue;
 
-		// 맵 구성
-		int islandCount = islandNumSet();
+                if (j >= M - 3 || map[i][j + 1] != 0 || map[i][j + 2] != 0)
+                    continue;
 
-		// 섬 거리 계산
-		edges = new int[islandCount][islandCount];
-		for (int i = 0; i < islandCount; i++)
-			Arrays.fill(edges[i], Integer.MAX_VALUE);
-		
-		// 섬 거리를 간선으로 만듬
-		islandListSet(islandCount);
+                int islandIdx = map[i][j];
+                for (int k = j + 3; k < M; k++) {
+                    if (map[i][k] == 0)
+                        continue;
+                    link[islandIdx][map[i][k]] = link[map[i][k]][islandIdx] = Math.min(link[islandIdx][map[i][k]], k - j - 1);
+                    j = k - 1;
+                    break;
 
-		// Prim으로 최소 사이클 거리 계산
-		int ans = calcBridgeLeastCost();
-		
-		System.out.println(ans == 0 ? -1 : ans);
+                }
+            }
+        }
+    }
 
-	}
-	static int islandNumSet() {
-		boolean[][] visit = new boolean[N][M];
-		int cnt = 0;
-		for (int i = 0; i < N; i++) {
-			for (int j = 0; j < M; j++) {
-				if (visit[i][j] || map[i][j] == 0)
-					continue;
-				Queue<Node> q = new LinkedList<>();
-				q.add(new Node(i, j));
-				visit[i][j] = true;
-				map[i][j] = ++cnt;
+    static void linkIslandsVertical() {
+        for (int j = 0; j < M; j++) {
+            for (int i = 0; i < N; i++) {
+                if (map[i][j] == 0)
+                    continue;
+                if (i >= N - 3 || map[i + 1][j] != 0 || map[i + 2][j] != 0)
+                    continue;
 
-				while (!q.isEmpty()) {
-					Node n = q.poll();
+                int islandIdx = map[i][j];
+                for (int k = i + 3; k < N; k++) {
+                    if (map[k][j] == 0)
+                        continue;
 
-					for (int d = 0; d < 4; d++) {
-						int nx = n.x + dxy[d][0];
-						int ny = n.y + dxy[d][1];
-						if (!mapChk(nx, ny) || visit[nx][ny] || map[nx][ny] != 1)
-							continue;
-						visit[nx][ny] = true;
-						map[nx][ny] = cnt;
-						q.add(new Node(nx, ny));
-					}
-				}
-			}
-		}
-		return cnt;
+                    link[islandIdx][map[k][j]] = link[map[k][j]][islandIdx] = Math.min(link[islandIdx][map[k][j]], k - i - 1);
+                    i = k - 1;
+                    break;
+                }
+            }
+        }
+    }
 
-	}
-	
-	static void islandListSet(int islandCount) {
-		LinkedList<Node> iList[] = new LinkedList[islandCount];
-		for (int i = 0; i < islandCount; i++)
-			iList[i] = new LinkedList<>();
+    static void mapNumbering() {
+        int islandIdx = 2;
 
-		for (int i = 0; i < N; i++)
-			for (int j = 0; j < M; j++)
-				if (map[i][j] != 0)
-					for (int d = 0; d < 4; d++)
-						iList[map[i][j] - 1].add(new Node(i, j, d, 0));
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                if (map[i][j] != 1)
+                    continue;
+                markIsland(i, j, islandIdx);
+                islandIdx++;
+            }
+        }
 
-		for (int i = 0; i < islandCount; i++)
-			islandDistanceSet(iList[i], i + 1);
-	}
+        for (int i = 0; i < N; i++)
+            for (int j = 0; j < M; j++)
+                map[i][j] = map[i][j] != 0 ? map[i][j] - 1 : 0;
 
-	static void islandDistanceSet(LinkedList<Node> list, int islandNum) {
-		Queue<Node> q = list;
+        link = new int[islandIdx - 1][islandIdx - 1];
+        for (int i = 0; i < link.length; i++)
+            Arrays.fill(link[i], Integer.MAX_VALUE);
+    }
 
-		while (!q.isEmpty()) {
-			Node n = q.poll();
+    static void markIsland(int x, int y, int islandIdx) {
 
-			int nx = n.x + dxy[n.d][0];
-			int ny = n.y + dxy[n.d][1];
+        map[x][y] = islandIdx;
 
-			if (!mapChk(nx, ny) || map[nx][ny] == islandNum)
-				continue;
-			if (map[nx][ny] != 0) {
-				if (n.cnt < 2)
-					continue;
-				if (edges[islandNum - 1][map[nx][ny] - 1] <= n.cnt)
-					continue;
-				edges[islandNum - 1][map[nx][ny] - 1] = n.cnt;
-				edges[map[nx][ny] - 1][islandNum - 1] = n.cnt;
-				continue;
-			}
-			q.add(new Node(nx, ny, n.d, n.cnt + 1));
+        boolean findFlag = true;
 
-		}
-	}
+        while (findFlag) {
+            findFlag = false;
 
-	// MST Prim
-	static int calcBridgeLeastCost() {
-		PriorityQueue<Node> pq = new PriorityQueue<>();
-		pq.add(new Node(0, 0)); // y : cnt
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j < M; j++) {
+                    if (map[i][j] != islandIdx)
+                        continue;
 
-		boolean[] visit = new boolean[edges.length];
-		int cnt = 0;
-		int ans = 0;
-		while (!pq.isEmpty()) {
-			Node n = pq.poll();
-			if (visit[n.x])
-				continue;
-			visit[n.x] = true;
-			ans += n.y;
-			for (int i = 0; i < edges[n.x].length; i++) {
-				if (visit[i] || edges[n.x][i] == Integer.MAX_VALUE)
-					continue;
-				pq.add(new Node(i, edges[n.x][i]));
-			}
-			// 정점 순회 끝
-			if (++cnt == edges.length)
-				break;
-		}
-		if(cnt != edges.length)
-			return 0;
-		return ans;
-	}
-	
-	static boolean mapChk(int x, int y) {
-		if (x < 0 || y < 0 || x >= N || y >= M)
-			return false;
-		return true;
-	}
+                    for (int d = 0; d < 4; d++) {
+                        int nx = i + dxy[d][0];
+                        int ny = j + dxy[d][1];
+
+                        if (!mapChk.apply(nx, ny) || map[nx][ny] != 1)
+                            continue;
+
+                        map[nx][ny] = islandIdx;
+                        findFlag = true;
+                    }
+                }
+            }
+        }
+    }
+
+
+    static BiFunction<Integer, Integer, Boolean> mapChk = (x, y) -> x >= 0 && y >= 0 && x < N && y < M;
+
+    static void input(BufferedReader br) throws IOException {
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        map = new int[N][M];
+        for (int i = 0; i < N; i++)
+            map[i] = Stream.of(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+    }
 }
