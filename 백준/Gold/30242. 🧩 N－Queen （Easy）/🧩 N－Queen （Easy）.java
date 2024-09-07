6ae7ac8cfe0ef00fe672789935dc217d51760334
chokim-01 <37997972+chokim-01@users.noreply.github.com>
2024-09-07 14:29:35 +0900

    [Gold II] Title: 🧩 N-Queen (Easy), Time: 148 ms, Memory: 15744 KB -BaekjoonHub

--- 백준/Gold/30242. 🧩 N－Queen （Easy）/🧩 N－Queen （Easy）.java
+++ 백준/Gold/30242. 🧩 N－Queen （Easy）/🧩 N－Queen （Easy）.java
@@ -7,7 +7,7 @@ import java.util.stream.Stream;
 public class Main {
     static int N;
     static int[] piece;
-    static HashSet[] v;
+    static boolean[][] v;
 
     static int[] ans;
 
@@ -17,16 +17,19 @@ public class Main {
         N = Integer.parseInt(br.readLine());
         piece = Stream.of(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
 
-        v = new HashSet[3];
-        for (int i = 0; i < 3; i++)
-            v[i] = new HashSet<>();
+        v = new boolean[3][];
+        for (int i = 0; i < 3; i++) {
+            v[0] = new boolean[N + 1];
+            v[1] = new boolean[N << 1 | 1];
+            v[2] = new boolean[N << 1 | 1];
+        }
 
         for (int i = 0; i < N; i++) {
             if (piece[i] == 0)
                 continue;
-            v[0].add(piece[i]);
-            v[1].add(N + i - piece[i]);
-            v[2].add(piece[i] + i);
+            v[0][piece[i]] = true;
+            v[1][N + i - piece[i]] = true;
+            v[2][piece[i] + i] = true;
         }
 
         dfs(0, new int[N]);
@@ -54,26 +57,24 @@ public class Main {
             choice[index] = 0;
         } else {
             for (int i = 1; i <= N; i++) {
-                if (v[0].contains(i) || v[1].contains(N + index - i) || v[2].contains(index + i))
+                if (v[0][i] || v[1][N + index - i] || v[2][index + i])
                     continue;
 
-                v[0].add(i);
-                v[1].add(N + index - i);
-                v[2].add(index + i);
+                v[0][i] = true;
+                v[1][N + index - i] = true;
+                v[2][index + i] = true;
                 choice[index] = i;
 
                 if (dfs(index + 1, choice))
                     return true;
 
-                v[0].remove(i);
-                v[1].remove(N + index - i);
-                v[2].remove(index + i);
+                v[0][i] = false;
+                v[1][N + index - i] = false;
+                v[2][index + i] = false;
                 choice[index] = 0;
             }
         }
 
         return false;
     }
-
-
 }
